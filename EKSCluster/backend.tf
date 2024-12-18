225025701cf7ae2d09a56286e5933336eb4d5584
mark <mark.alliston@outlook.com>
2024-12-18 20:27:27 +0000

    update

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" # CHANGE WITH YOUR S3 BUCKET
+    bucket = "mynews3test" # CHANGE WITH YOUR S3 BUCKET
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
